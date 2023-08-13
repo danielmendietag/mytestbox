48fbae1e97e1b8ae3dfa7c975830fdccdd828d31
FX Group Developer <fx.group.developer.1@gmail.com>
2023-08-12 21:44:53 -0600

    Create App

---
+++ android/app/src/main/kotlin/com/example/mytestbox/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mytestbox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const MyTestBox());
+}
+
+Future<void> initServices() async {}
+
+class MyTestBox extends StatelessWidget {
+  const MyTestBox({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: Get.deviceLocale,
+      title: "MYTEEBOX",
+    );
+  }
+}
