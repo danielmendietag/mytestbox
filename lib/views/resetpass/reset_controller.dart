ffad6755e8fc5b83310e363ba16eee620b3b8cf3
danielmendietag <daniel.mendieta@gmail.com>
2023-09-02 19:06:45 -0600

    Reset Password

---
+++ lib/views/resetpass/reset_controller.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+import '../../utils/logger.dart';
+
+class RecoverController extends GetxController {
+  var loading = false.obs;
+  final emailField = TextEditingController(text: '');
+
+  var storage = GetStorage('ordenservicio');
+
+  @override
+  void onInit() {
+    loadLogin();
+    super.onInit();
+  }
+
+  Future<void> loadLogin() async {
+    // var emailtr=storage.read("email");
+    // logger.i(emailtr);
+    // if(emailtr!=null){
+    //   emailField.text=emailtr.toString();
+    // }
+  }
+
+  FocusNode focusNodeEmail = FocusNode();
+}
---
+++ lib/views/resetpass/reset_password.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:mytestbox/views/resetpass/reset_controller.dart';
+import '../../config/app_themes.dart';
+import 'package:get/get.dart';
+import '../../widgets/recover_button.dart';
+import '../../widgets/recover_form.dart';
+
+class RecoverPage extends GetView<RecoverController> {
+  const RecoverPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        margin: const EdgeInsets.only(left: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 150,
+            ),
+            Text(
+              "Reset Password",
+              style: TextStyle(
+                height: 0,
+                color: Colors.black87,
+                fontSize: 24,
+                fontWeight: FontWeight.w900,
+                fontFamily: 'Muli',
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Please enter your email account",
+              style: TextStyle(
+                height: 0,
+                color: Colors.black87,
+                fontSize: 18,
+                fontWeight: FontWeight.normal,
+                fontFamily: 'Muli',
+              ),
+            ),
+            SizedBox(
+              height: 150,
+            ),
+            RecoverForm(),
+            SizedBox(
+              height: 20,
+            ),
+            recoverButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
