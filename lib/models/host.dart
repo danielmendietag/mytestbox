b946d8eaf4ecfe5815eb2b146721cbe9f2709dd6
danielmendietag <daniel.mendieta@gmail.com>
2023-09-02 19:03:39 -0600

    Host Model

---
+++ lib/models/host.dart
@@ -0,0 +1,6 @@
+import 'package:mytestbox/models/user.dart';
+
+class Host extends User {
+  Host(super.name, super.surname, super.nickname, super.createdAt,
+      super.updatedAt, super.id, super.handicap);
+}
