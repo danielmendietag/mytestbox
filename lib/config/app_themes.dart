bc689f1070570ced196b8a4d4e86509cc3f4c309
danielmendietag <daniel.mendieta@gmail.com>
2023-09-02 19:03:06 -0600

    More themes

--- lib/config/app_themes.dart
+++ lib/config/app_themes.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+const double kSpacing = 16.0;
+const kSpacerH = SizedBox(height: kSpacing);
 
 class AppThemes {
   static ThemeData main = ThemeData();
