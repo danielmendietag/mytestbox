672e0c411834c9858416235ac94a8394c6e66a23
danielmendietag <daniel.mendieta@gmail.com>
2023-09-17 15:26:41 -0600

    Spacer to adjust overflow

--- lib/widgets/logo_main.dart
+++ lib/widgets/logo_main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class LogoMainWidget extends StatelessWidget {
   const LogoMainWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const SizedBox(
@@ -9,10 +10,16 @@ class LogoMainWidget extends StatelessWidget {
       height: 300,
       child: Column(
         children: [
+          SizedBox(
+            height: 20,
+          ),
           Image(
-            image: AssetImage("assets/logo.png"),
-            width: 200,
-            height: 200,
+            image: AssetImage("assets/logo_main.png"),
+            width: 175,
+            height: 175,
+          ),
+          SizedBox(
+            height: 9,
           ),
           Text(
             'MYTEEBOX',
