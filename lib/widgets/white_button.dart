72e3b0af88a81554fba2a32529fbf33af522cdc7
danielmendietag <daniel.mendieta@gmail.com>
2023-09-17 15:27:47 -0600

    Get library

--- lib/widgets/white_button.dart
+++ lib/widgets/white_button.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 import '../views/signup/signup.dart';
 
 class WhiteButton extends StatelessWidget {
