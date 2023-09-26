768a9af2d99625cbbe2b0cdc3bd4e3511cdc2f87
Daniel Mendieta <daniel.mendieta@gmail.com>
2023-09-26 13:35:31 -0600

    Global Data 404

--- lib/services/global_data.dart
+++ lib/services/global_data.dart
@@ -1,39 +1,24 @@
 // import 'package:awesome_dialog/awesome_dialog.dart';
 // import 'package:flutter/material.dart';
 // import 'package:get/get.dart';
+// import 'package:mytestbox/models/post.dart';
+// import 'package:mytestbox/models/story.dart';
 // import 'package:mytestbox/models/user.dart';
+// import 'package:http/http.dart' as http;
+
+// import '../utils/logger.dart';
 
 // class GlobalData {
 //   static User user = User();
+//   static String token = '';
 
-//   void logout() {
-//     AwesomeDialog(
-//       dialogType: DialogType.info,
-//       animType: AnimType.topSlide,
-//       title: 'appName'.tr,
-//       desc: 'desea_salir'.tr,
-//       showCloseIcon: true,
-//       reverseBtnOrder: true,
-//       btnCancelText: 'no'.tr,
-//       btnCancelIcon: Icons.cancel,
-//       btnCancelColor: Colors.red,
-//       btnOkText: 'si'.tr,
-//       btnOkIcon: Icons.check_circle,
-//       btnOkColor: Colors.green,
-//       context: Get.context!,
-//       btnCancelOnPress: () {},
-//       btnOkOnPress: () {
-//         Get.offNamed('/login');
-//       },
-//     ).show();
+//   static void actualizarToken() {
+//     logger.i('Actualizar el token ($token)');
+//     if (token != null) {
+//       user.actualizarToken(token);
+//       logger.i('Actualizado el token ($token)');
+//     }
 //   }
 
-//   confirmBtn() {
-//     Get.back();
-//     Get.offNamed('/login');
-//   }
-
-//   cancelBtn() {
-//     Get.back();
-//   }
+//   final postUrl = '';
 // }
