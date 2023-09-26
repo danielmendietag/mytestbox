f5245cc406edc90a7e1c93c920cd1ca9f64f5798
Daniel Mendieta <daniel.mendieta@gmail.com>
2023-09-26 13:35:58 -0600

    Home Page and Controller

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,13 +1,221 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
 import 'package:get/get.dart';
-import 'home_controller.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:mytestbox/views/profile/profile.dart';
+// import 'package:mytestbox/widgets/search_form.dart';
+import '../../widgets/main_drawer.dart';
+import '../home/home_controller.dart';
 
 class HomePage extends GetView<HomeController> {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        toolbarHeight: 60,
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: true,
+        actions: <Widget>[
+          InkWell(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            onTap: () {
+              Get.to(ProfilePage());
+            },
+            child: Ink(
+              width: 60,
+              height: 60,
+              padding: EdgeInsets.only(left: 0, top: 0, right: 20, bottom: 0),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('assets/user_assets/profile1.jpeg'),
+              ),
+            ),
+          ),
+        ],
+        leading: IconButton(
+          padding: EdgeInsets.only(left: 30, top: 10),
+          icon: Image.asset('assets/btnLeftMenu.png'),
+          onPressed: () {
+            controller.openDrawer();
+          },
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: EdgeInsets.only(top: 10, bottom: 0, left: 0, right: 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text(
+                      'Find a host',
+                      style: TextStyle(
+                        fontFamily: 'Muli',
+                        fontSize: 30,
+                        fontWeight: FontWeight.w900,
+                        height: 0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Text(
+                            'Search a golf course',
+                            style: TextStyle(
+                              color: Color(0xFF6B6B6B),
+                              fontSize: 14,
+                              fontFamily: 'Mulish',
+                              fontWeight: FontWeight.w400,
+                              height: 0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 33),
+                          child: Container(
+                              width: 350,
+                              height: 57,
+                              padding: EdgeInsets.all(10),
+                              decoration: ShapeDecoration(
+                                  color: Color(0xFFF5F5F5),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(73))),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(73),
+                                    borderSide: BorderSide.none,
+                                  ),
+                                  suffixIcon: Image.asset('assets/search.png'),
+                                  hintText: 'eg: Mountain, Florest',
+                                  hintStyle: TextStyle(
+                                    fontSize: 14,
+                                    color: Color(0xFF6B6B6B),
+                                    fontFamily: 'Muli',
+                                    fontWeight: FontWeight.w400,
+                                    height: 0,
+                                  ),
+                                ),
+                              )),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              //Stories
+                              SingleChildScrollView(
+                                  scrollDirection: Axis.horizontal,
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: List.generate(
+                                      7,
+                                      (index) => Container(
+                                        padding: EdgeInsets.all(10),
+                                        child: Container(
+                                          decoration: ShapeDecoration(
+                                            shape: CircleBorder(),
+                                          ),
+                                          child: CircleAvatar(
+                                            radius: 25,
+                                            child: CircleAvatar(
+                                              backgroundImage: AssetImage(
+                                                  'assets/user_assets/profile3.jpeg'),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  )),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Stack(
+            children: <Widget>[
+              Expanded(
+                  flex: 4,
+                  child: Container(
+                    width: 410,
+                    height: 360,
+                    padding: EdgeInsets.only(
+                        left: 28, right: 20, top: 30, bottom: 20),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(246, 246, 246, 1),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30),
+                            bottomLeft: Radius.zero,
+                            bottomRight: Radius.zero)),
+                    child: Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Playing partners',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Mulish',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                              height: 0,
+                            ),
+                          ),
+                          IconButton(
+                            icon: Image.asset('assets/swipeup.png'),
+                            onPressed: () {
+                              print('Hello');
+                            },
+                          )
+                        ],
+                      ),
+                      //Scrollview to top
+                    ),
+                    //Posts
+                  )),
+              // SearchForm(),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/views/home/home_controller.dart
+++ lib/views/home/home_controller.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+// ignore: depend_on_referenced_packages
+import 'package:package_info_plus/package_info_plus.dart';
+import '../../widgets/main_drawer.dart';
+import '../../models/user.dart';
+import '../../models/host.dart';
+import '../../models/models.dart';
+import '../../models/post.dart';
+import '../../models/story.dart';
+import '../../utils/logger.dart';
 // import '../../services/global_data.dart';
+import '../../views/home/home.dart';
 
 class HomeController extends GetxController {
   var version = ''.obs;
   var loading = false.obs;
+  final _textController = TextEditingController();
+
+  @override
+  void onInit() async {
+    super.onInit();
+    // version.value = (await PackageInfo.fromPlatform()).version.toString();
+  }
 
   var scaffoldKey = GlobalKey<ScaffoldState>();
+
   void openDrawer() {
     scaffoldKey.currentState?.openDrawer();
   }
@@ -16,8 +33,24 @@ class HomeController extends GetxController {
     scaffoldKey.currentState?.openEndDrawer();
   }
 
-  // void logout() {
-  //   GlobalData O=GlobalData();
-  //   O.logout();
-  // }
+  void logout() {
+//     AwesomeDialog(
+//       dialogType: DialogType.info,
+//       animType: AnimType.topSlide,
+//       title: 'appName'.tr,
+//       desc: 'desea_salir'.tr,
+//       showCloseIcon: true,
+//       reverseBtnOrder: true,
+//       btnCancelText: 'no'.tr,
+//       btnCancelIcon: Icons.cancel,
+//       btnCancelColor: Colors.red,
+//       btnOkText: 'si'.tr,
+//       btnOkIcon: Icons.check_circle,
+//       btnOkColor: Colors.green,
+//       context: Get.context!,
+    // btnCancelOnPress: () {},
+    // btnOkOnPress: () {
+    //   Get.offNamed('/login');
+    // }.show();
+  }
 }
