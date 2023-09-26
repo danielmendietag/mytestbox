b28a2bdcf0d144bb966dc750d816192cf6f4ed6a
Daniel Mendieta <daniel.mendieta@gmail.com>
2023-09-26 13:33:35 -0600

    MHost 404

--- lib/models/host.dart
+++ lib/models/host.dart
@@ -1,6 +1,16 @@
-import 'package:mytestbox/models/user.dart';
+// import 'dart:ffi';
+// import 'package:awesome_dialog/awesome_dialog.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:get/get_core/src/get_main.dart';
+// import '../utils/logger.dart';
+// import 'package:mytestbox/models/user.dart';
 
-class Host extends User {
-  Host(super.name, super.surname, super.nickname, super.createdAt,
-      super.updatedAt, super.id, super.handicap);
-}
+// class Host extends User {
+//   Host(
+//     super.imageUrl, super.phoneNomber, {
+//     required super.objectId,
+//     required super.username, required super.createdAt, required super.updatedAt,
+//   });
+// }
