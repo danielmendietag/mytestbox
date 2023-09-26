c91d19b6c527d9fb6d727abc0f2afe9e432acda8
Daniel Mendieta <daniel.mendieta@gmail.com>
2023-09-26 13:34:46 -0600

    User 404

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,34 +1,82 @@
+// // import 'dart:ffi';
+
+// // import 'package:awesome_dialog/awesome_dialog.dart';
+// // import 'package:flutter/cupertino.dart';
+// // import 'package:flutter/material.dart';
+// // import 'package:get/get_core/src/get_main.dart';
+// // import '../utils/logger.dart';
+
+// // class UserList {
+// //   List<User>? results;
+
+// //   UserList({this.results});
+
+// //   UserList.fromJson(Map<String, dynamic> json) {
+// //     if (json['results'] != null) {
+// //       results = <User>[];
+// //       json['results'].forEach((v) {
+// //         results!.add(User.fromJson(v));
+// //       });
+// //     }
+// //   }
+
+// //   Map<String, dynamic> toJson() {
+// //     final Map<String, dynamic> data = <String, dynamic>{};
+// //     if (results != null) {
+// //       data['results'] = results!.map((v) => v.toJson()).toList();
+// //     }
+// //     return data;
+// //   }
+// // }
+
 class User {
-  int? handicap;
-  int? id;
-  String? name;
+  String? objectId;
+  final String username;
+  final String imageUrl;
+  String? password;
   String? surname;
-  String? nickname;
-  String? createdAt;
-  String? updatedAt;
+  final String phoneNomber;
+  bool? phoneVerified;
+  String createdAt;
+  String updatedAt;
 
-  User(String name, String surname, String nickname, String createdAt,
-      String updatedAt, int id, int handicap);
+  User(
+    this.imageUrl,
+    this.phoneNomber, {
+    required this.username,
+    this.password,
+    this.surname,
+    this.phoneVerified,
+    required this.createdAt,
+    required this.updatedAt,
+  });
 
-  User.fromJson(Map<String, dynamic> json) {
-    handicap = json["handicap"];
-    id = json["id"];
-    name = json["name"];
-    surname = json["surname"];
-    nickname = json["nickname"];
-    createdAt = json["createdAt"];
-    updatedAt = json["updatedAt"];
-  }
+//   User.fromJson(Map<String, dynamic> json) {
+//     objectId = json["objectId"];
+//     username = json["username"];
+//     password = json["password"];
+//     surname = json["surname"];
+//     phoneNomber = json["phoneNomber"];
+//     phoneVerified = json["phoneVerified"];
+//     createdAt = json["createdAt"];
+//     updatedAt = json["updatedAt"];
+//   }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data["handicap"] = handicap;
-    data["id"] = id;
-    data["name"] = name;
-    data["surname"] = surname;
-    data["nickname"] = nickname;
-    data["createdAt"] = createdAt;
-    data["updatedAt"] = updatedAt;
-    return data;
-  }
+//   Map<String, dynamic> toJson() {
+//     final Map<String, dynamic> data = <String, dynamic>{};
+//     data["objectId"] = objectId;
+//     data["username"] = username;
+//     data["surname"] = surname;
+//     data["phoneNomber"] = phoneNomber;
+//     data["phoneverified"] = phoneVerified;
+//     data["createdAt"] = createdAt;
+//     data["updatedAt"] = updatedAt;
+//     return data;
+//   }
+
+//   @override
+//   String toString() =>
+//       'User(objectId: $objectId, username: $username, surname: $surname, phoneNomber: $phoneNomber, phoneVerified: $phoneVerified, createdAt: $createdAt, upadtedAt: $updatedAt)';
+
+//   Future<void>
 }
