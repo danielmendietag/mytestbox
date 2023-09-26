9201d315e2331750458c8677d01cc6e5f9a36e82
Daniel Mendieta <daniel.mendieta@gmail.com>
2023-09-26 13:31:49 -0600

    Firebase Auth options and methods

---
+++ lib/services/firebase_auth.dart
@@ -0,0 +1,24 @@
+// import 'dart:ffi';
+
+// import "package:firebase_auth/firebase_auth.dart";
+// import 'package:flutter/material.dart';
+
+// class AuthMethods {
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+//   //Sign Up
+//   Future<String> signUpUser({
+//     required String phoneNo,
+//     required String password,
+//   }) async {
+//     String res = "Some error ocurred";
+//     try {
+//       if (phoneNo.isNotEmpty || password.isNotEmpty != null);
+//       //Registered user
+//       _auth.
+//     } catch (err) {
+//       res = err.toString();
+//     }
+//     return res;
+//   }
+// }
