ea00f45ae96ff28d64b68e2699c3ab3abf84426e
Daniel Mendieta <daniel.mendieta@gmail.com>
2023-09-26 13:34:27 -0600

    Story model

---
+++ lib/models/story.dart
@@ -0,0 +1,14 @@
+import 'package:mytestbox/models/user.dart';
+
+class Story {
+  final User user;
+  final String imageUrl;
+  final bool isViewed;
+
+  const Story({
+    required this.user,
+    required this.imageUrl,
+    this.isViewed = false,
+    required User username,
+  });
+}
