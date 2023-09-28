1519be86b59dce48781c44d017cc4f3a21533e32
Daniel Mendieta <daniel.mendieta@gmail.com>
2023-09-28 13:24:48 -0600

    Added MainDrawer

--- lib/views/notifications/notifications.dart
+++ lib/views/notifications/notifications.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/main_drawer.dart';
+
 class NotificationsPage extends StatelessWidget {
   const NotificationsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Notifications')),
+      appBar: AppBar(
+        title: Text('Notifications'),
+        centerTitle: true,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Builder(builder: (context) {
+            return IconButton(
+              onPressed: () => Scaffold.of(context).openDrawer(),
+              icon: Image.asset('assets/btnLeftMenu.png'),
+            );
+          }),
+        ),
+      ),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('Notifications'),
+      ),
     );
   }
 }
