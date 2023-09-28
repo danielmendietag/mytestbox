fd93580c85c44cc39ae32d815f3b257a65acb40b
Daniel Mendieta <daniel.mendieta@gmail.com>
2023-09-28 11:18:54 -0600

    Add What's new, ChatList and Notifications Page

--- lib/config/app_routes.dart
+++ lib/config/app_routes.dart
@@ -1,7 +1,12 @@
 import 'package:get/get.dart';
+import 'package:mytestbox/views/chatlist/chat_list.dart';
+import 'package:mytestbox/views/notifications/notifications.dart';
 // import 'package:mytestbox/config/app_binding.dart';
 import 'package:mytestbox/views/profile/profile.dart';
 import 'package:mytestbox/views/profile/profile_controller.dart';
+import 'package:mytestbox/views/whatsnew/whats_new.dart';
+import '../views/chatlist/chatlist_controller.dart';
+import '../views/notifications/notifications_controller.dart';
 import '../views/welcome/welcome.dart';
 import '../views/welcome/welcome_controller.dart';
 import '../views/signin/signin.dart';
@@ -14,6 +19,7 @@ import '../views/otpverif/otp_verif.dart';
 import '../views/otpverif/otp_controller.dart';
 import '../views/home/home.dart';
 import '../views/home/home_controller.dart';
+import '../views/whatsnew/whatsnew_controller.dart';
 
 class AppRoutes {
   static final routes = [
@@ -64,6 +70,34 @@ class AppRoutes {
       page: () => const ProfilePage(),
       binding: BindingsBuilder(() {
         Get.lazyPut<ProfileController>(() => ProfileController());
+      }),
+    ),
+    GetPage(
+      name: '/profile',
+      page: () => const ProfilePage(),
+      binding: BindingsBuilder(() {
+        Get.lazyPut<ProfileController>(() => ProfileController());
+      }),
+    ),
+    GetPage(
+      name: '/whatsnew',
+      page: () => const WhatsNewPage(),
+      binding: BindingsBuilder(() {
+        Get.lazyPut<WhatsNewController>(() => WhatsNewController());
+      }),
+    ),
+    GetPage(
+      name: '/chatlist',
+      page: () => const ChatListPage(),
+      binding: BindingsBuilder(() {
+        Get.lazyPut<ChatListController>(() => ChatListController());
+      }),
+    ),
+    GetPage(
+      name: '/notifications',
+      page: () => const NotificationsPage(),
+      binding: BindingsBuilder(() {
+        Get.lazyPut<NotificationsController>(() => NotificationsController());
       }),
     ),
   ];
