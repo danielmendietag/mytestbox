262b0e2fda81b94f8168980cee4da63af86d1234
Daniel Mendieta <daniel.mendieta@gmail.com>
2023-09-28 11:25:11 -0600

    Notifications Page & Controller

---
+++ lib/views/notifications/notifications_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotificationsController extends StatelessWidget {
+  const NotificationsController({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
