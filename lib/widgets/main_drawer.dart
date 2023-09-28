26f0b2398480f0f494f612d2893cd53865332328
Daniel Mendieta <daniel.mendieta@gmail.com>
2023-09-28 11:28:13 -0600

    MainDrawer

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,63 +1,152 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter/cupertino.dart';
+import '../config/app_routes.dart';
+import '../views/Aboutus/about_us.dart';
+import '../views/chatlist/chat_list.dart';
+import '../views/notifications/notifications.dart';
+import '../views/whatsnew/whats_new.dart';
 
-class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+class MainDrawer extends StatefulWidget {
+  @override
+  _MainDrawerState createState() => _MainDrawerState();
+}
 
+class _MainDrawerState extends State<MainDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            child: Text('Myteebox'),
-            decoration: BoxDecoration(
-              color: Colors.black,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFF000000),
+              Color(0xFF3A6433),
+            ],
+          ),
+        ),
+        child: ListView(
+          padding: EdgeInsets.only(left: 10, bottom: 0, top: 40, right: 0),
+          children: <Widget>[
+            Row(
+              children: [
+                Image.asset(
+                  'assets/logo_main.png',
+                  scale: 3.0,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 0),
+                      child: Text(
+                        'MYTEEBOX',
+                        style: TextStyle(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            fontFamily: 'Bungee',
+                            fontSize: 28,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 0),
+                      child: Text(
+                        '.golf',
+                        style: TextStyle(
+                            color: Color(0xFF9ED493),
+                            fontFamily: 'Bungee',
+                            fontSize: 24,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ),
-          ListTile(
-            title: Text('Home'),
-            tileColor: Get.currentRoute == '/' ? Colors.grey[300] : null,
-            onTap: () {
-              print(Get.currentRoute);
-              Get.back();
-              Get.offNamed('/');
-            },
-          ),
-          ListTile(
-            title: Text('Become a Host'),
-            tileColor:
-                Get.currentRoute == '/become_host' ? Colors.grey[300] : null,
-            onTap: () {
-              print(Get.currentRoute);
-              Get.back();
-              Get.offNamed('/become_host');
-            },
-          ),
-          ListTile(
-            title: Text('Playing partners'),
-            tileColor: Get.currentRoute == '/playing_partners'
-                ? Colors.grey[300]
-                : null,
-            onTap: () {
-              print(Get.currentRoute);
-              Get.back();
-              Get.offNamed('/playing_partners');
-            },
-          ),
-          ListTile(
-            title: Text('Settings'),
-            tileColor:
-                Get.currentRoute == '/settings' ? Colors.grey[300] : null,
-            onTap: () {
-              print(Get.currentRoute);
-              Get.back();
-              Get.offNamed('/settings');
-            },
-          ),
-        ],
+            SizedBox(
+              height: 30,
+            ),
+            ListTile(
+              title: Text(
+                'MYTEEBOX',
+                style: TextStyle(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    fontFamily: 'Bungee',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400),
+              ),
+              tileColor:
+                  Get.currentRoute == '/aboutus' ? Colors.grey[300] : null,
+              onTap: () {
+                Get.to(AboutUsPage());
+              },
+            ),
+            ListTile(
+              title: Text(
+                'What´s new',
+                style: TextStyle(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    fontFamily: 'Muli',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400),
+              ),
+              tileColor:
+                  Get.currentRoute == '/whatsnew' ? Colors.grey[300] : null,
+              onTap: () {
+                print(Get.currentRoute);
+                Get.back();
+                Get.to(WhatsNewPage());
+              },
+            ),
+            ListTile(
+              title: Text(
+                'Message Center',
+                style: TextStyle(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    fontFamily: 'Muli',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400),
+              ),
+              tileColor:
+                  Get.currentRoute == '/chatlist' ? Colors.grey[300] : null,
+              onTap: () {
+                print(Get.currentRoute);
+                Get.back();
+                Get.to(ChatListPage());
+              },
+            ),
+            ListTile(
+              title: Text(
+                'Notifications',
+                style: TextStyle(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    fontFamily: 'Muli',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400),
+              ),
+              tileColor: Get.currentRoute == '/notifications'
+                  ? Colors.grey[300]
+                  : null,
+              onTap: () {
+                print(Get.currentRoute);
+                Get.back();
+                Get.to(NotificationsPage());
+              },
+              leading: Icon(
+                Icons.circle,
+                color: Colors.red,
+                size: 18,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
